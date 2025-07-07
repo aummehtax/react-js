3532a3fe0243cfd6fd7b0ba3262c114d45982130
Mehta Aum <maum5302@gmail.com>
2025-07-08 01:07:49 +0530

    Intro to React

---
+++ Day 01/my-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import Food from "./components/Food";
+import Footer from "./components/Footer";
+import Header from "./components/Header"; 
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Food />
+      <Food />
+      <Food />
+      <Footer />
+    </> 
+    
+  );
+}
+
+export default App;
---
+++ Day 01/my-react-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return(
+        <footer>
+            <hr />
+            <p>@copy {new Date().getFullYear()} This is my Website</p>
+        </footer>
+    )
+}
+
+export default Footer
